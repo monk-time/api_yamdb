6c133dfaeea59208b09242741ec82b081fe4a162
JRushFobos <90862284+JRushFobos@users.noreply.github.com>
2023-05-10 19:01:59 +0500

    Feature/titles (#19)
    * Добавление моделей Title, Сategory, Genre
    и структуры api
    
    * Добавление сериализаторов...
    - CategoriesSerializer
    - GenresSerializer
    - TitlesPostSerializer
    - TitlesPostSerializer
    
    * Создание моделей, сериализаторов, вьюх, урлов для
    - Названий произведений
    - Названий жанров произведений
    - Названий категорий произведений
    pytest пишет о 6 ошибках:
    FAILED tests/test_02_category.py::Test02CategoryAPI::test_02_category_with_admin_user - AssertionError: Проверьте, что для эндпоинта `/api/v1/categories/` настроена пагинация. Значение параметра `results` отсутствует или содержит некорректную информацию о существующем объекте.
    FAILED tests/test_02_category.py::Test02CategoryAPI::test_04_category_delete_admin - AssertionError: Проверьте, что DELETE-запрос администратора к `/api/v1/categories/{slug}/` возвращает ответ со статусом 204.
    FAILED tests/test_03_genre.py::Test03GenreAPI::test_02_genre - AssertionError: Проверьте, что для эндпоинта `/api/v1/genres/` настроена пагинация. Значение параметра `results` отсутствует или содержит некорректную информацию о существующем объекте.
    FAILED tests/test_03_genre.py::Test03GenreAPI::test_04_genres_delete - AssertionError: Проверьте, что DELETE-запрос администратора к `/api/v1/genres/{slug}/` возвращает ответ со  статусом 204.
    FAILED tests/test_04_title.py::Test04TitleAPI::test_02_title_admin - AttributeError: 'Title' object has no attribute 'reviews'
    FAILED tests/test_04_title.py::Test04TitleAPI::test_03_titles_detail - AttributeError: 'Title' object has no attribute 'reviews'
    
    * bugfix_v1
    List bugs for next bug fix:
    FAILED tests/test_02_category.py::Test02CategoryAPI::test_04_category_delete_admin - AssertionError: Проверьте, что DELETE-запрос администратора к `/api/v1/categories/{slug}/` возвращает ответ со статусом 204.
    FAILED tests/test_03_genre.py::Test03GenreAPI::test_04_genres_delete - AssertionError: Проверьте, что DELETE-запрос администратора к `/api/v1/genres/{slug}/` возвращает ответ со  статусом 204.
    FAILED tests/test_04_title.py::Test04TitleAPI::test_02_title_admin - AttributeError: 'Title' object has no attribute 'reviews'
    FAILED tests/test_04_title.py::Test04TitleAPI::test_03_titles_detail - AttributeError: 'Title' object has no attribute 'reviews'
    
    * bugfix_v2
    
    * bugfix_v2.1

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    'django_filters',
     'reviews',
     'api',
 ]
@@ -120,6 +121,9 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5,
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
 }
 
 SIMPLE_JWT = {
