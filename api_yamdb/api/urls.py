8ac718137d1154fe81939b82eaf0870d4f6d23d3
JRushFobos <90862284+JRushFobos@users.noreply.github.com>
2023-05-10 19:01:59 +0500

    Feature/titles (#19)
    * Добавление моделей Title, Сategory, Genre
    и структуры api
    
    * Добавление сериализаторов...
    - CategoriesSerializer
    - GenresSerializer
    - TitlesPostSerializer
    - TitlesPostSerializer
    
    * Создание моделей, сериализаторов, вьюх, урлов для
    - Названий произведений
    - Названий жанров произведений
    - Названий категорий произведений
    pytest пишет о 6 ошибках:
    FAILED tests/test_02_category.py::Test02CategoryAPI::test_02_category_with_admin_user - AssertionError: Проверьте, что для эндпоинта `/api/v1/categories/` настроена пагинация. Значение параметра `results` отсутствует или содержит некорректную информацию о существующем объекте.
    FAILED tests/test_02_category.py::Test02CategoryAPI::test_04_category_delete_admin - AssertionError: Проверьте, что DELETE-запрос администратора к `/api/v1/categories/{slug}/` возвращает ответ со статусом 204.
    FAILED tests/test_03_genre.py::Test03GenreAPI::test_02_genre - AssertionError: Проверьте, что для эндпоинта `/api/v1/genres/` настроена пагинация. Значение параметра `results` отсутствует или содержит некорректную информацию о существующем объекте.
    FAILED tests/test_03_genre.py::Test03GenreAPI::test_04_genres_delete - AssertionError: Проверьте, что DELETE-запрос администратора к `/api/v1/genres/{slug}/` возвращает ответ со  статусом 204.
    FAILED tests/test_04_title.py::Test04TitleAPI::test_02_title_admin - AttributeError: 'Title' object has no attribute 'reviews'
    FAILED tests/test_04_title.py::Test04TitleAPI::test_03_titles_detail - AttributeError: 'Title' object has no attribute 'reviews'
    
    * bugfix_v1
    List bugs for next bug fix:
    FAILED tests/test_02_category.py::Test02CategoryAPI::test_04_category_delete_admin - AssertionError: Проверьте, что DELETE-запрос администратора к `/api/v1/categories/{slug}/` возвращает ответ со статусом 204.
    FAILED tests/test_03_genre.py::Test03GenreAPI::test_04_genres_delete - AssertionError: Проверьте, что DELETE-запрос администратора к `/api/v1/genres/{slug}/` возвращает ответ со  статусом 204.
    FAILED tests/test_04_title.py::Test04TitleAPI::test_02_title_admin - AttributeError: 'Title' object has no attribute 'reviews'
    FAILED tests/test_04_title.py::Test04TitleAPI::test_03_titles_detail - AttributeError: 'Title' object has no attribute 'reviews'
    
    * bugfix_v2
    
    * bugfix_v2.1

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,15 +1,25 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import AuthTokenView, SignUpView, UserViewSet
+from .views import (
+    AuthTokenView,
+    CategoryViewSet,
+    GenreViewSet,
+    SignUpView,
+    TitleViewSet,
+    UserViewSet,
+)
 
 app_name = 'api'
 
 router_v1 = DefaultRouter()
 router_v1.register(r'users', UserViewSet, basename='user')
+router_v1.register('titles', TitleViewSet, basename='title')
+router_v1.register('genres', GenreViewSet, basename='genre')
+router_v1.register('categories', CategoryViewSet, basename='category')
 
 urlpatterns = [
-    path('v1/', include(router_v1.urls)),
     path('v1/auth/signup/', SignUpView.as_view(), name='signup'),
     path('v1/auth/token/', AuthTokenView.as_view(), name='auth_token'),
+    path('v1/', include(router_v1.urls)),
 ]
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
 
-from .validators import validate_username_not_me
+from .validators import validate_username_not_me, validate_year
 
 
 class User(AbstractUser):
@@ -48,3 +48,70 @@ class User(AbstractUser):
     @property
     def is_moderator(self):
         return self.role == self.MODERATOR
+
+
+class Genre(models.Model):
+    """Модель жанра произведения"""
+
+    name = models.CharField(max_length=256, verbose_name='Жанр')
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    """Модель категории произведения"""
+
+    name = models.CharField(max_length=256, verbose_name='Категория')
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    """Модель названия произведения"""
+
+    name = models.CharField(max_length=256, verbose_name='Название')
+    year = models.IntegerField(
+        validators=[validate_year], verbose_name='Год выпуска'
+    )
+    description = models.TextField(verbose_name='Описание', blank=True)
+    genre = models.ManyToManyField(
+        Genre,
+        related_name='titles',
+        verbose_name='Slug жанра',
+        help_text='Жанр, к которому будет относиться произведение',
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='titles',
+        verbose_name='Slug категории',
+        help_text='Категория, к которому будет относиться произведение',
+    )
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return self.name
