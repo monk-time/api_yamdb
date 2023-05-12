1afc55b013d59dbb7a1730a6c764ba5a4d5fe56f
LenarFazlyev <121536686+LenarFazlyev@users.noreply.github.com>
2023-05-12 23:31:51 +0300

    Feature/rating (#27)

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
+from django.db.models import Avg
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
@@ -121,7 +122,9 @@ class UserViewSet(ModelViewSet):
 class TitleViewSet(ModelViewSet):
     """Вьюсет названия произведения"""
 
-    queryset = Title.objects.all()
+    queryset = Title.objects.annotate(rating=Avg('reviews__score')).order_by(
+        'name'
+    )
     permission_classes = (IsAdminOrReadOnly,)
     filter_backends = (DjangoFilterBackend,)
     filterset_class = TitleFilter
