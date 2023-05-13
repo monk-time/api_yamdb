fad7edd9f1ba7cbc33857ca369eb2c70607834cf
Dmitry <monk-time@users.noreply.github.com>
2023-05-13 22:44:41 +0300

    Create new app "users" and fix comments

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,122 +1,26 @@
-from django.conf import settings
-from django.contrib.auth.tokens import default_token_generator
-from django.core.mail import send_mail
 from django.db.models import Avg
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import status
-from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
 from rest_framework.mixins import (
     CreateModelMixin,
     DestroyModelMixin,
     ListModelMixin,
 )
-from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
-from rest_framework_simplejwt.tokens import AccessToken
 
-from reviews.models import Category, Genre, Review, Title, User
+from reviews.models import Category, Genre, Review, Title
 
 from .filters import TitleFilter
-from .permissions import (
-    IsAdminOrReadOnly,
-    IsAdminOrSuper,
-    IsStaffOrAuthorOrReadOnly,
-)
+from .permissions import IsAdminOrReadOnly, IsStaffOrAuthorOrReadOnly
 from .serializers import (
     CategorySerializer,
     CommentSerializer,
     GenreSerializer,
     ReviewSerializer,
-    SignUpSerializer,
     TitleReadSerializer,
     TitleWriteSerializer,
-    TokenSerializer,
-    UserMeSerializer,
-    UserSerializer,
 )
-
-
-class SignUpView(APIView):
-    permission_classes = (AllowAny,)
-
-    @staticmethod
-    def send_confirmation_code(token: str, email: str):
-        send_mail(
-            subject='Код подтверждения для API YaMDb',
-            message=f'Ваш код подтверждения: {token}',
-            from_email=settings.EMAIL_HOST_USER,
-            recipient_list=[email],
-        )
-
-    def post(self, request):
-        serializer = SignUpSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-
-        if isinstance(serializer.validated_data, User):
-            user = serializer.validated_data
-        else:
-            user = serializer.save()
-
-        token = default_token_generator.make_token(user)
-        self.send_confirmation_code(token, user.email)
-        return Response(serializer.data)
-
-
-class AuthTokenView(APIView):
-    permission_classes = (AllowAny,)
-
-    def post(self, request):
-        serializer = TokenSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-
-        username = serializer.validated_data['username']
-        confirmation_code = serializer.validated_data['confirmation_code']
-        user = get_object_or_404(User, username=username)
-
-        if not default_token_generator.check_token(user, confirmation_code):
-            return Response(
-                {"confirmation_code": ["Код подтверждения неверный"]},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-
-        access_token = AccessToken.for_user(user)
-        return Response({'token': str(access_token)})
-
-
-class UserViewSet(ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = (IsAdminOrSuper,)
-    filter_backends = (SearchFilter,)
-    search_fields = ('username',)
-    lookup_field = 'username'
-    http_method_names = ('get', 'post', 'patch', 'delete')
-
-    def get_serializer_class(self):
-        if self.action in ('me', 'patch_me'):
-            return UserMeSerializer
-        return self.serializer_class
-
-    @action(
-        detail=False,
-        permission_classes=(IsAuthenticated,),
-    )
-    def me(self, request):
-        serializer = self.get_serializer(request.user)
-        return Response(serializer.data)
-
-    @me.mapping.patch
-    def patch_me(self, request):
-        serializer = self.get_serializer(
-            request.user, data=request.data, partial=True
-        )
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
 
 
 class TitleViewSet(ModelViewSet):
