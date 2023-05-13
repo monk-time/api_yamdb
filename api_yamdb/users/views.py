9289e0fc49ebd869dbd329534327b7ecd6299e8b
Dmitry <monk-time@users.noreply.github.com>
2023-05-13 22:53:12 +0300

    Remove get_serializer_class from UserViewSet

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -77,13 +77,9 @@ class UserViewSet(ModelViewSet):
     lookup_field = 'username'
     http_method_names = ('get', 'post', 'patch', 'delete')
 
-    def get_serializer_class(self):
-        if self.action in ('me', 'patch_me'):
-            return UserMeSerializer
-        return self.serializer_class
-
     @action(
         detail=False,
+        serializer_class=UserMeSerializer,
         permission_classes=(IsAuthenticated,),
     )
     def me(self, request):
