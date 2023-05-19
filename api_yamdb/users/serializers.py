b7967f9360d7639604f0019a2613173e341ea3b1
Dmitry <monk-time@users.noreply.github.com>
2023-05-19 14:59:23 +0000

    Change registration validation (#37)
    * Change registration validation
    
    * Add Github Action

--- api_yamdb/users/serializers.py
+++ api_yamdb/users/serializers.py
@@ -1,27 +1,39 @@
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from rest_framework import serializers
+from rest_framework.validators import ValidationError
 
-from .models import User
+from .models import EMAIL_MAX_LENGTH, USERNAME_MAX_LENGTH, User
+from .validators import validate_username_not_me
 
 USER_FIELDS = ('username', 'email', 'first_name', 'last_name', 'bio', 'role')
 
 
 class SignUpSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(max_length=EMAIL_MAX_LENGTH)
+    username = serializers.CharField(
+        max_length=USERNAME_MAX_LENGTH,
+        validators=[UnicodeUsernameValidator(), validate_username_not_me],
+    )
+
     class Meta:
         model = User
         fields = ('username', 'email')
 
-    def to_internal_value(self, data):
-        email = data.get('email')
-        username = data.get('username')
-
-        if email and username:
-            try:
-                user = User.objects.get(email=email, username=username)
-                return user
-            except User.DoesNotExist:
-                pass
-
-        return super().to_internal_value(data)
+    def create(self, validated_data):
+        username = validated_data['username']
+        email = validated_data['email']
+        user_by_username = User.objects.filter(username=username).first()
+        user_by_email = User.objects.filter(email=email).first()
+        if not any((user_by_username, user_by_email)):
+            return User.objects.create(**validated_data)
+        if user_by_username == user_by_email:
+            return user_by_username
+        response = {}
+        if user_by_username:
+            response['username'] = ['Already used']
+        if user_by_email:
+            response['email'] = ['Already used']
+        raise ValidationError(response)
 
 
 class TokenSerializer(serializers.Serializer):
